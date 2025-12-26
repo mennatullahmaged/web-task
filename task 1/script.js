35462d08fd0458496300edb53668148e91ac3a41
Mennatullah Maged <menna.majed@outlook.com>
2025-12-26 21:24:54 +0200

    Add files via upload

---
+++ task 1/script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("infoBtn");
+const message = document.getElementById("message");
+
+button.onclick = function () {
+    message.innerHTML =
+        "Fun Fact: Cloud computing helps people play games, watch videos, and use apps from anywhere in the world! ☁️🌍";
+};
